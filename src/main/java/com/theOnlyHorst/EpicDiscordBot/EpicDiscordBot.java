94c5f3df26aa4db87fb1de888c444502ab00cf78
theOnlyHorst <buschsteffmichi@chello.at>
2018-11-23 15:54:37 +0100

    Made a Ping Pong bot as test

--- src/main/java/com/theOnlyHorst/EpicDiscordBot/EpicDiscordBot.java
+++ src/main/java/com/theOnlyHorst/EpicDiscordBot/EpicDiscordBot.java
@@ -1,9 +1,35 @@
 package com.theOnlyHorst.EpicDiscordBot;
 
-public class EpicDiscordBot {
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-    public static void main(String[] args)
+import javax.security.auth.login.LoginException;
+
+public class EpicDiscordBot extends ListenerAdapter {
+
+    public static void main(String[] args) throws LoginException, RateLimitedException {
+        new JDABuilder(AccountType.BOT).setToken("NTE1NTI3NTg3MTE2NjEzNjQz.DtmaZw.-50l870oJQjlvdYHCDoC7RzbLMA").addEventListener(new EpicDiscordBot()).buildAsync();
+
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
     {
-        
+        if (event.getAuthor().isBot()) return;
+        // We don't want to respond to other bot accounts, including ourself
+        Message message = event.getMessage();
+        String content = message.getRawContent();
+        // getContentRaw() is an atomic getter
+        // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
+        if (content.equals("!ping"))
+        {
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
+        }
     }
 }
